d712e208d7a8c6634860a011607ba26a3220923c
Jamie Garner <jamiegarner123@gmail.com>
2020-05-30 10:41:47 +0800

    cleanup

--- src/features/game/CircleOfWords.js
+++ src/features/game/CircleOfWords.js
@@ -5,6 +5,8 @@ import styled from "styled-components";
 import { MediumLargeText, TEXT_TOP_PADDING } from "../../components/text/Text";
 import colors from "../../theme/colors";
 import { screenWidth } from "../../utils/sizing-utils";
+import { containerColors } from "./game-utils";
+// import { State, PanGestureHandler } from "react-native-gesture-handler";
 
 const ContentContainer = styled(View)`
   flex: 1;
@@ -14,13 +16,15 @@ const ContentContainer = styled(View)`
   padding: 5%;
 `;
 
+const RADIUS = (screenWidth - 120) / 2;
+
 const Circle = styled(View)`
-  height: ${screenWidth - 120};
-  width: ${screenWidth - 120};
+  height: ${RADIUS * 2};
+  width: ${RADIUS * 2};
   border-width: 1;
   border-color: red;
   position: relative;
-  border-radius: ${screenWidth - 120};
+  border-radius: ${RADIUS};
 `;
 
 const WordContainer = styled(View)`
@@ -36,49 +40,6 @@ const WordText = styled(MediumLargeText)`
   padding-top: ${5 + TEXT_TOP_PADDING};
   padding-bottom: 5;
 `;
-
-const containerColors = [
-  "rgba(234,82,60,1)",
-  "rgba(234,190,60,1)",
-  "rgba(57,188,218,1)",
-  "rgba(147,205,74,1)",
-  "rgba(246,167,193,1)",
-  "rgba(175,110,78,1)",
-  "rgba(56,144,143,1)",
-  "rgba(188,133,163,1)",
-  "rgba(234,82,60,1)",
-  "rgba(234,190,60,1)",
-  "rgba(57,188,218,1)",
-  "rgba(147,205,74,1)",
-  "rgba(246,167,193,1)",
-  "rgba(175,110,78,1)",
-  "rgba(56,144,143,1)",
-  "rgba(188,133,163,1)",
-  "rgba(234,82,60,1)",
-  "rgba(234,190,60,1)",
-  "rgba(57,188,218,1)",
-  "rgba(147,205,74,1)",
-  "rgba(246,167,193,1)",
-  "rgba(175,110,78,1)",
-  "rgba(56,144,143,1)",
-  "rgba(188,133,163,1)",
-  "rgba(234,82,60,1)",
-  "rgba(234,190,60,1)",
-  "rgba(57,188,218,1)",
-  "rgba(147,205,74,1)",
-  "rgba(246,167,193,1)",
-  "rgba(175,110,78,1)",
-  "rgba(56,144,143,1)",
-  "rgba(188,133,163,1)",
-  "rgba(234,82,60,1)",
-  "rgba(234,190,60,1)",
-  "rgba(57,188,218,1)",
-  "rgba(147,205,74,1)",
-  "rgba(246,167,193,1)",
-  "rgba(175,110,78,1)",
-  "rgba(56,144,143,1)",
-  "rgba(188,133,163,1)",
-];
 
 const PositionAbsoluteWord = ({ onPress, onLayout, word }) => {
   return (
@@ -126,21 +87,10 @@ const getCircleCoordinatesForAngle = (angle, radius) => {
 
 const CircleOfWords = ({ words }) => {
   const [wordState, setWordState] = useState(getInitialWordState(words));
-  const [circleLayout, setCircleLayout] = useState({
-    radius: null,
-    centre: { x: null, y: null },
-  });
   const [positionsSet, setPositionsSet] = useState(false);
 
   const onPressWord = word => {
     console.log(">>> word: ", word);
-  };
-
-  const onCircleLayout = event => {
-    const { x, y, width } = event.nativeEvent.layout;
-    const radius = width / 2;
-    const centre = { x: x + radius, y: y + radius };
-    setCircleLayout({ radius, centre });
   };
 
   const onWordLayout = (event, word) => {
@@ -152,25 +102,25 @@ const CircleOfWords = ({ words }) => {
   };
 
   useEffect(() => {
-    if (!positionsSet && circleLayout.radius && wordState.every(w => w.width)) {
+    if (!positionsSet && wordState.every(w => w.width)) {
       setPositionsSet(true);
 
       const clonedWordState = cloneDeep(wordState);
       clonedWordState.forEach((w, i) => {
-        const { xCoord, yCoord } = getCircleCoordinatesForAngle(w.angle, circleLayout.radius);
+        const { xCoord, yCoord } = getCircleCoordinatesForAngle(w.angle, RADIUS);
         const halfWidth = w.width / 2;
         const halfHeight = w.height / 2;
-        const startingX = circleLayout.radius - halfWidth;
+        const startingX = RADIUS - halfWidth;
         const startingY = -1 * halfHeight;
         clonedWordState[i] = { ...w, x: startingX + xCoord, y: startingY + yCoord };
       });
       setWordState(clonedWordState);
     }
-  }, [circleLayout, wordState]);
+  }, [wordState]);
 
   return (
     <ContentContainer>
-      <Circle onLayout={onCircleLayout}>
+      <Circle>
         {wordState.map((w, i) => (
           <PositionAbsoluteWord onLayout={onWordLayout} onPress={onPressWord} word={w} />
         ))}
--- src/features/game/game-utils.js
+++ src/features/game/game-utils.js
@@ -6,3 +6,14 @@ const getMiddleColor = (color1, color2) => {
   const middle = rgb1.map((c, i) => ((c + rgb2[i]) / 2) * 1.3);
   return `rgba(${middle[0]}, ${middle[1]}, ${middle[2]}, 1)`;
 };
+
+export const containerColors = [
+  "rgba(234,82,60,1)",
+  "rgba(234,190,60,1)",
+  "rgba(57,188,218,1)",
+  "rgba(147,205,74,1)",
+  "rgba(246,167,193,1)",
+  "rgba(175,110,78,1)",
+  "rgba(56,144,143,1)",
+  "rgba(188,133,163,1)",
+];
