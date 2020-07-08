bb344e8222726a63833e4cee68786025c3111a59
Jamie Garner <jamiegarner123@gmail.com>
2020-07-08 10:19:54 +0800

    stage button stuff

--- src/features/stage-selection/StageButton.js
+++ src/features/stage-selection/StageButton.js
@@ -1,34 +1,40 @@
 import React from "react";
+import { TouchableOpacity, View, Image } from "react-native";
 import styled from "styled-components";
-import { TouchableOpacity, View } from "react-native";
 import { SmallMediumText } from "../../components/text/Text";
 import { SCREENS } from "../../app-constants";
 
+const IMAGE_SIZE = 100;
+
 const ButtonContainer = styled(View)`
   width: 33.3%;
-`;
-
-const ButtonInnerContainer = styled(View)`
-  width: 100%;
-  aspect-ratio: 1;
+  height: ${IMAGE_SIZE};
+  margin-vertical: 12;
 `;
 
 const Button = styled(TouchableOpacity)`
   flex: 1;
-  border-width: 1;
-  border-color: red;
-  margin-vertical: 12;
+  width: ${IMAGE_SIZE};
+  height: ${IMAGE_SIZE};
   margin-horizontal: 12;
+`;
+
+const StageImage = styled(Image)`
+  width: ${IMAGE_SIZE};
+  height: ${IMAGE_SIZE};
+  border-radius: 20;
 `;
 
 const StageButton = ({ changeScreen }) => {
   return (
     <ButtonContainer>
-      <ButtonInnerContainer>
-        <Button onPress={() => changeScreen(SCREENS.GAME)}>
-          <SmallMediumText color="black">Play</SmallMediumText>
-        </Button>
-      </ButtonInnerContainer>
+      <Button onPress={() => changeScreen(SCREENS.GAME)}>
+        <StageImage
+          source={require("../../components/stage-background/canyon.jpg")}
+          resizeMode="cover"
+        />
+        {/* <SmallMediumText color="black">Play</SmallMediumText> */}
+      </Button>
     </ButtonContainer>
   );
 };
